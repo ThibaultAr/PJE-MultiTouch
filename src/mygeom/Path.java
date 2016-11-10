0b50efc0fea877e4eec93901d10902db4974dc3b
Yann Dubois <ttreplay@gmail.com>
2016-11-10 19:50:50 +0100

    Add isobarycentre in Path

--- src/mygeom/Path.java
+++ src/mygeom/Path.java
@@ -25,6 +25,18 @@ public class Path {
 		this.points.clear();
 	}
 	
+	protected Point2 isoBarycentre() {
+		double sumX = 0;
+		double sumY = 0;
+
+		for(Point2 p : this.points) {
+			sumX += p.getX();
+			sumY += p.getY();
+		}
+		
+		return new Point2(sumX / this.points.size(), sumY / this.points.size());
+	}
+	
 	public void draw(Graphics2D g2) {
 		GeneralPath p = new GeneralPath();
 		if(this.points.isEmpty())
@@ -35,9 +47,12 @@ public class Path {
 		}
 		
 		Point2 lastPoint = this.points.get(this.points.size()-1);
+		Point2 isoBarycentre = this.isoBarycentre();
 		
 		g2.draw(new Ellipse2D.Double(lastPoint.getX()* Main.SURFACE_WIDTH -10, lastPoint.getY() * Main.SURFACE_HEIGHT -10, 20, 20));
 		g2.drawString("" + cursorId, (float) (lastPoint.getX()* Main.SURFACE_WIDTH - 3), (float) (lastPoint.getY() * Main.SURFACE_HEIGHT -2));
+		
+		g2.fill(new Ellipse2D.Double(isoBarycentre.getX() * Main.SURFACE_WIDTH - 5, isoBarycentre.getY() * Main.SURFACE_HEIGHT - 5, 10, 10));
 		
 		g2.draw(p);
 	}
