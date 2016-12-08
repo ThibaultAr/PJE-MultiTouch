92c42f43f9bb68f6ee924cc402dcd1c3fcf9588e
Thibault ARLOING <arloing@P2-AUTOMATIQUE.LOCAL>
2016-12-08 17:29:40 +0100

    Recognize

--- src/oneDollarRecognizer/OneDollarRecognizer.java
+++ src/oneDollarRecognizer/OneDollarRecognizer.java
@@ -1,13 +1,12 @@
 package oneDollarRecognizer;
 
+import java.util.Vector;
+
 import mygeom.Path;
 import mygeom.Point2;
+import mygeom.Tuple2;
 
 public class OneDollarRecognizer {
-	public GestureEvent recognize(Path points) {
-		return null;
-	}
-
 	public double distance(Point2 p1, Point2 p2) {
 		double distX = p2.getX() - p1.getX();
 		double distY = p2.getY() - p1.getY();
@@ -54,16 +53,94 @@ public class OneDollarRecognizer {
 
 		return this.rotateBy(points, -teta);
 	}
-	
+
 	public Path rotateBy(Path points, double angle) {
 		Point2 c = points.isoBarycentre();
 		Path newPoints = new Path();
-		for(Point2 p : points.getPoints()) {
+		for (Point2 p : points.getPoints()) {
 			double qx = (p.getX() - c.getX()) * Math.cos(angle) - (p.getY() - c.getY()) * Math.sin(angle) + c.getX();
 			double qy = (p.getX() - c.getX()) * Math.cos(angle) + (p.getY() - c.getY()) * Math.sin(angle) + c.getX();
-			
+
 			newPoints.add(new Point2(qx, qy));
 		}
 		return newPoints;
+	}
+
+	public Path scaleToSquare(Path points, int size) {
+		double bWidth = this.boundingWidth(points);
+		double bHeight = this.boundingHeight(points);
+		Path newPoints = new Path();
+		for (Point2 p : points.getPoints()) {
+			double qx = p.getX() * (size / bWidth);
+			double qy = p.getY() * (size / bWidth);
+			newPoints.add(new Point2(qx, qy));
+		}
+		return newPoints;
+	}
+
+	private double boundingWidth(Path points) {
+		double minX = points.getPoints().get(0).getX();
+		double maxX = points.getPoints().get(0).getX();
+		for (Point2 p : points.getPoints()) {
+			if (p.getX() > maxX)
+				maxX = p.getX();
+			if (p.getX() < minX)
+				minX = p.getX();
+		}
+		return maxX - minX;
+	}
+
+	private double boundingHeight(Path points) {
+		double minY = points.getPoints().get(0).getY();
+		double maxY = points.getPoints().get(0).getY();
+		for (Point2 p : points.getPoints()) {
+			if (p.getY() > maxY)
+				maxY = p.getY();
+			if (p.getY() < minY)
+				minY = p.getY();
+		}
+		return maxY - minY;
+	}
+
+	public Path translateToOrigin(Path points) {
+		Point2 c = points.isoBarycentre();
+		Path newPoints = new Path();
+		for (Point2 p : points.getPoints()) {
+			double qx = p.getX() - c.getX();
+			double qy = p.getY() - c.getY();
+			newPoints.add(new Point2(qx, qy));
+		}
+		return newPoints;
+	}
+
+	public GestureEvent recognize(Path points, Vector<Template> templates, int size) {
+		double b = Double.MAX_VALUE;
+		Template t2 = templates.get(0);
+		Point2 c = points.isoBarycentre();
+		double teta = Math.atan2(c.getY() - points.getPoints().get(0).getY(),
+				c.getX() - points.getPoints().get(0).getX());
+		for (Template t : templates) {
+			double d = this.distanceAtAngle(points, t);
+			if (d < b) {
+				b = d;
+				t2 = t;
+			}
+		}
+		double score = 1 - b / (0.5 * Math.sqrt(size * size + size * size));
+		return new GestureEvent(null, 0, t2.getName(), score);
+	}
+
+	public double distanceAtAngle(Path points, Template t) {
+		Path newPoints = this.rotateToZero(points);
+		double d = this.pathDistance(newPoints, t.getPoints());
+		return d;
+	}
+
+	public double pathDistance(Path points, Vector<Tuple2> tpoints) {
+		double d = 0;
+		for (int i = 0; i < points.getPoints().size(); i++) {
+			d += this.distance(points.getPoints().get(i), new Point2(tpoints.get(i).getX(), tpoints.get(i).getY()));
+		}
+		return d / points.getPoints().size();
 	}
 }
