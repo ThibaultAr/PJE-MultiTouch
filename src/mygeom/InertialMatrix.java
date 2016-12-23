d3f42dd92dfeb115ed2747de74fb233c1ac45faa
Yann Dubois <ttreplay@gmail.com>
2016-12-23 19:37:04 +0100

    Determination of the obb's points

--- src/mygeom/InertialMatrix.java
+++ src/mygeom/InertialMatrix.java
@@ -1,6 +1,8 @@
 package mygeom;
 
 import java.awt.Color;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Point2D;
 
 import main.Main;
 
@@ -72,15 +74,75 @@ public class InertialMatrix {
 		double r1 = (a + b - Math.sqrt((a - b) * (a - b) + 4 * (f * f))) / 2;
 		double r2 = (a + b + Math.sqrt((a - b) * (a - b) + 4 * (f * f))) / 2;
 		
-		Vector2 u = new Vector2(-f, -a + r1).normalize().mul(50);
-		Vector2 v = new Vector2(-f, -a + r2).normalize().mul(50);
+		Vector2 u = new Vector2(-f, -a + r1).normalize();
+		Vector2 v = new Vector2(-f, -a + r2).normalize();
 		
-		centroid.x *= Main.SURFACE_WIDTH;
-		centroid.y *= Main.SURFACE_HEIGHT;
 		
-		DebugDraw.add(new Segment2(centroid, u), 3, Color.red);
-		DebugDraw.add(new Segment2(centroid, v), 3, Color.green);
-		  
+		Point2 c = new Point2(centroid);
+		c.x *= Main.SURFACE_WIDTH;
+		c.y *= Main.SURFACE_HEIGHT;
+		
+		DebugDraw.add(new Segment2(c, new Vector2(u).mul(50)), 3, Color.red);
+		DebugDraw.add(new Segment2(c, new Vector2(v).mul(50)), 3, Color.green);
+		
+		AffineTransform transform = new AffineTransform();
+		transform.setToTranslation(centroid.x, centroid.y);
+				
+		double dot = new Vector2(1,0).dot(v);
+		double det = new Vector2(1,0).determinant(v);
+		
+		if(dot > 1) dot = 1;
+		
+		double angle = Math.signum(det) * Math.acos(dot);
+		transform.rotate(angle);
+		
+		double scale = u.length() / new Vector2(1,0).length();
+		transform.scale(scale, scale);
+		
+		double minX = Double.MAX_VALUE;
+		double maxX = Double.MIN_VALUE;
+		double minY = Double.MAX_VALUE;
+		double maxY = Double.MIN_VALUE;
+
+		for(Point2 p : this.path.getPoints()) {
+			if (p.getX() > maxX)
+				maxX = p.getX();
+			if (p.getX() < minX)
+				minX = p.getX();
+			if (p.getY() > maxY)
+				maxY = p.getY();
+			if (p.getY() < minY)
+				minY = p.getY();
+		}
+		
+		double width = maxX - minX;
+		double height = maxY - minY;
+		
+		Point2D x1 = new Point2D.Double(-width / 2, -height / 2);
+		Point2D x2 = new Point2D.Double(width / 2, -height / 2);
+		Point2D y1 = new Point2D.Double(-width / 2, height / 2);
+		Point2D y2 = new Point2D.Double(width / 2, height / 2);
+		
+		Point2D x1P = new Point2D.Double();
+		Point2D x2P = new Point2D.Double();
+		Point2D y1P = new Point2D.Double();
+		Point2D y2P = new Point2D.Double();
+		
+		transform.transform(x1, x1P);
+		transform.transform(x2, x2P);
+		transform.transform(y1, y1P);
+		transform.transform(y2, y2P);
+		
+		Point2 p1 = new Point2(x1P.getX() * Main.SURFACE_WIDTH, x1P.getY() * Main.SURFACE_HEIGHT);
+		Point2 p2 = new Point2(x2P.getX() * Main.SURFACE_WIDTH, x2P.getY() * Main.SURFACE_HEIGHT);
+		Point2 p3 = new Point2(y1P.getX() * Main.SURFACE_WIDTH, y1P.getY() * Main.SURFACE_HEIGHT);
+		Point2 p4 = new Point2(y2P.getX() * Main.SURFACE_WIDTH, y2P.getY() * Main.SURFACE_HEIGHT);
+		
+		DebugDraw.add(new Segment2(p1, p2), 3, Color.black);
+		DebugDraw.add(new Segment2(p1, p3), 3, Color.black);
+		DebugDraw.add(new Segment2(p2, p4), 3, Color.black);
+		DebugDraw.add(new Segment2(p3, p4), 3, Color.black);
+		
 		return obb;
 	}
 }
